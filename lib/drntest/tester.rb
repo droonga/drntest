4b467f502f8836742f6341703ddd891f2964ef36
Yoji Shidara <dara@shidara.net>
2013-09-18 17:34:22 +0900

    Show diff on failure

--- lib/drntest/tester.rb
+++ lib/drntest/tester.rb
@@ -1,6 +1,8 @@
 require "optparse"
 require "json"
 require "droonga/client"
+require "tempfile"
+require "pp"
 
 module Drntest
   class Tester
@@ -78,10 +80,7 @@ module Drntest
           puts "PASS"
         else
           puts "FAIL"
-          puts "Expected:"
-          p expected
-          puts "Actual:"
-          p actual
+          show_diff(expected, actual)
           reject_path = target_path.sub_ext(".reject")
           puts "Saving received result as #{reject_path}"
           File.write(reject_path, actual_json)
@@ -91,6 +90,29 @@ module Drntest
         puts "No expectation specified. Saving result as #{actual_path}."
         File.write(actual_path, actual_json)
       end
+    end
+
+    def show_diff(expected, actual)
+      expected_pretty = expected.pretty_inspect
+      actual_pretty = actual.pretty_inspect
+
+      create_temporary_file("expected", expected_pretty) do |expected_file|
+        create_temporary_file("actual", actual_pretty) do |actual_file|
+          diff_options = [
+            "-u",
+            "--label", "(expected)", expected_file.path,
+            "--label", "(actual)", actual_file.path
+          ]
+          system("diff", *diff_options)
+        end
+      end
+    end
+
+    def create_temporary_file(key, content)
+      file = Tempfile.new("drntest-#{key}")
+      file.write(content)
+      file.close
+      yield(file)
     end
 
     def normalize_result(result)
