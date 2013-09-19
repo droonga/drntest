fce8dfb4ab5024a9560cd5264862fe464db46673
Yoji Shidara <dara@shidara.net>
2013-09-19 12:12:52 +0900

    Extract TestRunner

---
+++ lib/drntest/test-runner.rb
@@ -0,0 +1,73 @@
+module Drntest
+  class TestRunner
+    attr_reader :tester, :target
+
+    def initialize(tester, target)
+      @tester = tester
+      @target = target
+    end
+
+    def run
+      client = Droonga::Client.new(tag: tester.tag, port: tester.port)
+      envelope = JSON.parse(File.read(target))
+      target_path = Pathname(target)
+      expected_path = target_path.sub_ext(".expected")
+
+      print "#{target}: "
+      actual = client.connection.send_receive(envelope)
+      unless actual
+        puts "No response received"
+        return
+      end
+      actual = normalize_result(actual)
+      actual_json = actual.to_json
+
+      if File.exist?(expected_path)
+        expected = JSON.parse(File.read(expected_path))
+        expected = normalize_result(expected)
+        if expected == actual
+          puts "PASS"
+        else
+          puts "FAIL"
+          show_diff(expected, actual)
+          reject_path = target_path.sub_ext(".reject")
+          puts "Saving received result as #{reject_path}"
+          File.write(reject_path, actual_json)
+        end
+      else
+        actual_path = target_path.sub_ext(".actual")
+        puts "No expectation specified. Saving result as #{actual_path}."
+        File.write(actual_path, actual_json)
+      end
+    end
+
+    def show_diff(expected, actual)
+      expected_pretty = expected.pretty_inspect
+      actual_pretty = actual.pretty_inspect
+
+      create_temporary_file("expected", expected_pretty) do |expected_file|
+        create_temporary_file("actual", actual_pretty) do |actual_file|
+          diff_options = [
+            "-u",
+            "--label", "(expected)", expected_file.path,
+            "--label", "(actual)", actual_file.path
+          ]
+          system("diff", *diff_options)
+        end
+      end
+    end
+
+    def create_temporary_file(key, content)
+      file = Tempfile.new("drntest-#{key}")
+      file.write(content)
+      file.close
+      yield(file)
+    end
+
+    def normalize_result(result)
+      result = result.dup
+      result[1] = 0 # Mask start time
+      result
+    end
+  end
+end
--- lib/drntest/tester.rb
+++ lib/drntest/tester.rb
@@ -3,6 +3,7 @@ require "json"
 require "droonga/client"
 require "tempfile"
 require "pp"
+require "drntest/test-runner"
 
 module Drntest
   class Tester
@@ -54,7 +55,8 @@ module Drntest
     def run(*targets)
       tests = load_tests(*targets)
       tests.each do |test|
-        run_test(test)
+        test_runner = TestRunner.new(self, test)
+        test_runner.run
       end
       0 # FIXME
     end
@@ -71,69 +73,6 @@ module Drntest
         end
       end
       tests
-    end
-
-    def run_test(target)
-      client = Droonga::Client.new(tag: tag, port: port)
-      envelope = JSON.parse(File.read(target))
-      target_path = Pathname(target)
-      expected_path = target_path.sub_ext(".expected")
-
-      print "#{target}: "
-      actual = client.connection.send_receive(envelope)
-      unless actual
-        puts "No response received"
-        return
-      end
-      actual = normalize_result(actual)
-      actual_json = actual.to_json
-
-      if File.exist?(expected_path)
-        expected = JSON.parse(File.read(expected_path))
-        expected = normalize_result(expected)
-        if expected == actual
-          puts "PASS"
-        else
-          puts "FAIL"
-          show_diff(expected, actual)
-          reject_path = target_path.sub_ext(".reject")
-          puts "Saving received result as #{reject_path}"
-          File.write(reject_path, actual_json)
-        end
-      else
-        actual_path = target_path.sub_ext(".actual")
-        puts "No expectation specified. Saving result as #{actual_path}."
-        File.write(actual_path, actual_json)
-      end
-    end
-
-    def show_diff(expected, actual)
-      expected_pretty = expected.pretty_inspect
-      actual_pretty = actual.pretty_inspect
-
-      create_temporary_file("expected", expected_pretty) do |expected_file|
-        create_temporary_file("actual", actual_pretty) do |actual_file|
-          diff_options = [
-            "-u",
-            "--label", "(expected)", expected_file.path,
-            "--label", "(actual)", actual_file.path
-          ]
-          system("diff", *diff_options)
-        end
-      end
-    end
-
-    def create_temporary_file(key, content)
-      file = Tempfile.new("drntest-#{key}")
-      file.write(content)
-      file.close
-      yield(file)
-    end
-
-    def normalize_result(result)
-      result = result.dup
-      result[1] = 0 # Mask start time
-      result
     end
   end
 end
